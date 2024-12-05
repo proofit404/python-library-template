cdd770898fc0c675af333f10499a62bfa640738d
Josiah Kaviani <proofit404@gmail.com>
2024-12-05 21:36:41 +0400

    chore: proprietary license #175

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,6 +8,9 @@ def main():
 
     Path("README.md").symlink_to("docs/index.md")
 
+    if "{% if cookiecutter.is_private %}T{% endif %}":
+        Path("LICENSE").unlink()
+
 
 if __name__ == "__main__":
     main()
